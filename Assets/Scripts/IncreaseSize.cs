3631c8d2d74e2fd563b3443c45ebc5ca3b512f06
Bonart1104 <chshepindn@gmail.com>
2023-10-15 17:08:43 +0700

    fix

--- Assets/Scripts/IncreaseSize.cs
+++ Assets/Scripts/IncreaseSize.cs
@@ -6,8 +6,6 @@ public class IncreaseSize : MonoBehaviour
     [SerializeField] private float _minSize;
     [SerializeField] private float _maxSize;
 
-    private float _size;
-
     private void Update()
     {
             transform.localScale = new Vector3(Mathf.PingPong(Time.time * _speed, _maxSize - _minSize) + _minSize, Mathf.PingPong(Time.time * _speed, _maxSize - _minSize) + _minSize, Mathf.PingPong(Time.time * _speed, _maxSize - _minSize) + _minSize);
