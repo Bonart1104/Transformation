5da275fdd9c5ac55e1fd386f56f22d63d3965548
Bonart1104 <chshepindn@gmail.com>
2023-10-15 12:47:10 +0700

    New names

--- Assets/Scripts/MovingForward.cs
+++ Assets/Scripts/MovingForward.cs
@@ -6,8 +6,8 @@ public class Move : MonoBehaviour
 
     void Update()
     {
-        transform.localScale += new Vector3(Time.deltaTime * _speed, Time.deltaTime * _speed, Time.deltaTime * _speed);
         transform.Translate(Time.deltaTime * _speed, 0, 0);
-        transform.Rotate(0, Time.deltaTime * _speed, 0);
+        //transform.localScale += new Vector3(Time.deltaTime * _speed, Time.deltaTime * _speed, Time.deltaTime * _speed);
+        //transform.Rotate(0, Time.deltaTime * _speed, 0);
     }
 }
